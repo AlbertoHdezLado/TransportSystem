3e163a5ef7518f7bd10cb4dad161c07e8cde412d
Alberto Hernández Lado <albertohljob@gmail.com>
2022-06-03 04:27:29 +0200

    Firebase configuration and adding navigation graphs

--- app/src/main/java/com/example/android/transportsystem/LoginFragment.kt
+++ app/src/main/java/com/example/android/transportsystem/LoginFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +15,16 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val v = inflater.inflate(R.layout.fragment_login, container, false)
+        val log_login_button = v.findViewById<Button>(R.id.reg_registerbutton)
+        log_login_button.setOnClickListener{
+            val navHostFragment =
+                activity!!.supportFragmentManager.findFragmentById(R.id.init_navigation) as NavHostFragment
+            val navController = navHostFragment.navController
+            navController.navigate(R.id.registerFragment)
+
+        }
+
+        return v
     }
 }
