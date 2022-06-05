c8a0ccaab2f93aece321f20978aaf6114081145c
Alberto Hernández Lado <albertohljob@gmail.com>
2022-06-05 05:21:43 +0200

    Trying to read stations from firebase

--- app/src/main/java/com/example/android/transportsystem/MainActivity.kt
+++ app/src/main/java/com/example/android/transportsystem/MainActivity.kt
@@ -1,14 +1,51 @@
 package com.example.android.transportsystem
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val collectionReference : CollectionReference = db.collection("users")
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = findViewById<TextView>(R.id.main_currentName)
+        val money = findViewById<TextView>(R.id.main_currentMoney)
+
+        val auth = FirebaseAuth.getInstance()
+        val email = auth.currentUser?.email
+        val db = FirebaseFirestore.getInstance()
+        val usersRef = db.collection("users")
+        usersRef.document(email!!).get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document.exists()) {
+                    name.text = "${document.getString("name").toString()} ${document.getString("surname").toString()}"
+                    money.text = "${document.getDouble("money").toString()} pln"
+                } else {
+                    Log.d(TAG, "The document doesn't exist.")
+                }
+            } else {
+                task.exception?.message?.let {
+                    Log.d(TAG, "Error in task")
+                }
+            }
+        }
+
+
     }
 
     //To override onBackPressed on MainFragment
--- app/src/main/java/com/example/android/transportsystem/PayFragment.kt
+++ app/src/main/java/com/example/android/transportsystem/PayFragment.kt
@@ -1,10 +1,24 @@
 package com.example.android.transportsystem
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.QueryDocumentSnapshot
+
+
+
+
 
 class PayFragment : Fragment() {
 
@@ -15,8 +29,48 @@ class PayFragment : Fragment() {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_pay, container, false)
 
-        //TODO
+        val rootRef = FirebaseFirestore.getInstance()
+        val subjectsRef = rootRef.collection("stations")
+
+        val subjects: MutableList<String> = ArrayList()
+        subjectsRef.get().
+        addOnCompleteListener { documents ->
+            if (documents.isSuccessful) {
+                for (document in documents.result) {
+                    subjects.add(document.id)
+                }
+            } else {
+                Log.d("ERROR", "Error reading firebase data", documents.exception)
+            }
+        }
+
+        val spinner = v.findViewById<Spinner>(R.id.pay_initialstation)
+
+        if (spinner != null) {
+            val adapter = ArrayAdapter(v.context,
+                android.R.layout.simple_spinner_item, subjects)
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long,
+                ) {
+                    Toast.makeText(activity,
+                                subjects[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+
+
+        }
 
         return v
     }
+
+
 }
