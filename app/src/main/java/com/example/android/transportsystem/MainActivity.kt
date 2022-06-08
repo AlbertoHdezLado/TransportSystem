51dd5e3fa8b9b508ad3155edd666b25af9c2ce23
Alberto Hernández Lado <albertohljob@gmail.com>
2022-06-08 19:01:29 +0200

    Update

--- app/src/main/java/com/example/android/transportsystem/MainActivity.kt
+++ app/src/main/java/com/example/android/transportsystem/MainActivity.kt
@@ -2,31 +2,14 @@ package com.example.android.transportsystem
 
 import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.renderscript.ScriptGroup
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.TextView
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import com.example.android.transportsystem.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
